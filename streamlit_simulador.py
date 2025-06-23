3c0c77cdb7600b6b9802b22f6f77112ec6d4cd60
Ânderson Matheus Flores De Oliveira <90271653+MySpaceCrazy@users.noreply.github.com>
2025-06-23 09:06:07 -0300

    Update streamlit_simulador.py

--- streamlit_simulador.py
+++ streamlit_simulador.py
@@ -10,7 +10,7 @@ import pytz
 
 st.set_page_config(page_title="Simulador de Separação de Produtos", layout="wide")
 
-col_titulo, col_botao, col_vazio = st.columns([5, 5, 5])
+col_titulo, col_botao, col_vazio = st.columns([5, 5, 2])
 
 with col_titulo:
     st.title("🧪 Simulador de Separação de Produtos")
